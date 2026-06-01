72cc5ecf0411cdd8cfeb59c6e577c95394c7bb6a
aaryan9009 <167018645+aaryan9009@users.noreply.github.com>
2026-06-01 12:14:25 +0530

    Update team names and logos in teamsData

--- lib/teamsData.ts
+++ lib/teamsData.ts
@@ -1,47 +1,93 @@
 // lib/teamsData.ts
 
+// export const INITIAL_TEAMS = [
+//   {
+//     _id: "t1",
+//     name: "रौद्रशंभू योद्धास",
+//     owner: "NAVJEET PATIL",
+//     logo: "/logos/logo.png!bw800",
+//     pointsUsed: 0
+//   },
+//   {
+//     _id: "t2",
+//     name: "माऊली वॉरियर्स",
+//     owner: "PANKAJ PATIL",
+//     logo: "/logos/logo.png!bw800",
+//     pointsUsed: 0
+//   },
+//   {
+//     _id: "t3",
+//     name: "राजमुद्रा वॉरियर्स",
+//     owner: "ADARSH PATIL",
+//     logo: "/logos/logo.png!bw800",
+//     pointsUsed: 0
+//   },
+//   {
+//     _id: "t4",
+//     name: "शिवमुद्रा वॉरियर्स",
+//     owner: "LAKHAN PATIL",
+//     logo: "/logos/logo.png!bw800",
+//     pointsUsed: 0
+//   },
+//   {
+//     _id: "t5",
+//     name: "स्वराज्य वॉरियर्स",
+//     owner: "ANIL CHOUGULE",
+//     logo: "/logos/logo.png!bw800",
+//     pointsUsed: 0
+//   },
+//   {
+//     _id: "t6",
+//     name: "सह्याद्री फायटर्स",
+//     owner: "PRUTHVIRAJ",
+//     logo: "/logos/logo.png!bw800",
+//     pointsUsed: 0
+//   }
+// ];
+
+
+
 export const INITIAL_TEAMS = [
   {
     _id: "t1",
-    name: "रौद्रशंभू योद्धास",
+    name: "Panthers",
     owner: "NAVJEET PATIL",
-    logo: "/logos/logo.png!bw800",
+    logo: "/logos/Panthers.png",
     pointsUsed: 0
   },
   {
     _id: "t2",
-    name: "माऊली वॉरियर्स",
+    name: "Titans",
     owner: "PANKAJ PATIL",
-    logo: "/logos/logo.png!bw800",
+    logo: "/logos/Titans.png",
     pointsUsed: 0
   },
   {
     _id: "t3",
-    name: "राजमुद्रा वॉरियर्स",
+    name: "Warriors",
     owner: "ADARSH PATIL",
-    logo: "/logos/logo.png!bw800",
+    logo: "/logos/Warriors.png",
     pointsUsed: 0
   },
   {
     _id: "t4",
-    name: "शिवमुद्रा वॉरियर्स",
+    name: "Spartans",
     owner: "LAKHAN PATIL",
-    logo: "/logos/logo.png!bw800",
+    logo: "/logos/Spartans.png",
     pointsUsed: 0
   },
   {
     _id: "t5",
-    name: "स्वराज्य वॉरियर्स",
+    name: "Thunder",
     owner: "ANIL CHOUGULE",
-    logo: "/logos/logo.png!bw800",
+    logo: "/logos/Thunder.png",
     pointsUsed: 0
   },
   {
     _id: "t6",
-    name: "सह्याद्री फायटर्स",
-    owner: "PRUTHVIRAJ",
-    logo: "/logos/logo.png!bw800",
+    name: "Strikers",
+    owner: "PRATIK PATIL",
+    logo: "/logos/Strikers.png",
     pointsUsed: 0
   }
-  
 ];
